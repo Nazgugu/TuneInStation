bcf5624c1368d8875c605a4f68333e1c4d42bf32
Nazgugu <prada520farrell@gmail.com>
2016-01-11 17:26:18 -0800

    added cocoapods

---
+++ Podfile
@@ -0,0 +1,7 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '8.0'
+inhibit_all_warnings!
+
+pod 'AFNetworking',     '~>3.0.4'
+pod 'SDWebImage', 	'~>3.7.3'
+pod 'WSProgressHUD', '~>1.0.9'
---
+++ TuneInStation/ValueKeyDefine.h
@@ -0,0 +1,14 @@
+//
+//  ValuKeyDefine.h
+//  myCapacity
+//
+//  Created by Liu Zhe on 10/22/15.
+//  Copyright © 2015 Liu Zhe. All rights reserved.
+//
+
+#ifndef ValueKeyDefine_h
+#define ValueKeyDefine_h
+
+
+
+#endif /* ValueKeyDefine_h */
